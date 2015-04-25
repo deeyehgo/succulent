fc1f5ab34dfd41bb32383b03512c491823f767e4
Diego <diego@upperquad.com>
2015-04-25 01:48:29 -0700

    WIP CustomImage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 (function () {
   // Fetch data
+  // TODO: parse location - query buckets?
   var accessToken = '885512ce4b18bbb6132f2c553f716838de07a5e0a49c866213946f5a092ba68e';
-  var url = 'https://api.dribbble.com/v1/shots?access_token=' + accessToken;
+  var url = 'https://api.dribbble.com/v1/buckets/266815/shots?access_token=' + accessToken;
   var xhr = new XMLHttpRequest();
   var data = {};
   xhr.responseType = 'json';
@@ -22,25 +23,27 @@
   // TODO: move
   var content = document.querySelector('.content');
   function createImageElements() {
-    var currentImage = document.createElement('img');
-    var nextImage = document.createElement('img');
+    var currentImgElement = new Image();
+    var nextImgElement = new Image();
 
-    currentImage.src = data[0].images.hidpi || data[0].images.normal;
-    nextImage.src = data[1].images.hidpi || data[1].images.normal;
+    currentImgElement.src = data[0].images.hidpi || data[0].images.normal;
+    nextImgElement.src = data[1].images.hidpi || data[1].images.normal;
 
-    currentImage.addEventListener('load', handleImageLoaded);
-    currentImage.addEventListener('progress', handleImageProgress);
-    nextImage.addEventListener('load', handleImageLoaded);
-    nextImage.addEventListener('progress', handleImageProgress);
+    currentImgElement.addEventListener('load', handleImageLoaded);
+    currentImgElement.addEventListener('progress', handleImageProgress);
+    nextImgElement.addEventListener('load', handleImageLoaded);
+    nextImgElement.addEventListener('progress', handleImageProgress);
     
-    content.appendChild(currentImage);
-    content.appendChild(nextImage);
+    content.appendChild(currentImgElement);
+    content.appendChild(nextImgElement);
 
-    // for (var dataItem in data) {
-    //   if (data.hasOwnProperty(dataItem)) {
-    //     console.log(data[dataItem]);
-    //   }
-    // }
+
+    for (var dataItem in data) {
+      if (data.hasOwnProperty(dataItem)) {
+        console.log(data[dataItem]);
+      }
+    }
+    createTitle();
   }
 
   function handleImageLoaded(event) {
@@ -50,6 +53,55 @@
   function handleImageProgress(event) {
     console.log('image progress', event);
   }
+
+  function createTitle(text) {
+    // var titleElement = document.createElement('div');
+    // titleElement.innerHTML = text;
+    // content.appendChild(titleElement);
+    var customImg = new CustomImage();
+    customImg.init({
+      content: content,
+      displayText: data[0].title || '',
+      src: data[0].images.hidpi || data[0].images.normal || ''
+    });
+    content.appendChild(customImg.element());
+    // console.log(customImg.createTitle());
+  }
+
+  function CustomImage() {
+    var _element = '';
+  }
+
+  CustomImage.prototype.init = function(options) {
+    var options = options || {};
+    
+    var content = options.content || document.body;
+    var img = new Image();
+    var titleTextElement = document.createElement('div');
+
+    img.src = options.src || '';
+    titleTextElement.innerHTML = options.displayText || '';
+    _element = document.createElement('div');
+    _element.appendChild(titleTextElement);
+    console.dir(options);
+  };
+
+  CustomImage.prototype.element = function() {
+    return _element;
+  };
+
+  CustomImage.prototype.setTitle = function(displayText) {
+    console.log(displayText, this.displayText);
+    return this;
+  };
+
+  CustomImage.prototype.show = function(fromRight) {
+    
+  };
+
+  CustomImage.prototype.hide = function(fromRight) {
+    
+  };
 
   // Build img
   // Composite img + title
